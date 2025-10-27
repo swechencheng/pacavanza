c7a270ffb555f5f207dd5b420f1fc9621ab30c5a
Chencheng Zhang <charles.cc.zhang@gmail.com>
2025-10-27 10:41:25 +0100

    Listen to all network interfaces

--- pacavanza/dashboard/app_chart.py
+++ pacavanza/dashboard/app_chart.py
@@ -137,4 +137,4 @@ class ChartApp:
             return fig
 
     def run(self):
-        self.app.run(debug=False, use_reloader=False, port=self.port)
+        self.app.run(debug=False, use_reloader=False, host="0.0.0.0", port=self.port)
